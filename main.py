0a8d3fa7e1738c6b7555ef20e4d7826a1c294fad
zack0712 <108528390+zack0712@users.noreply.github.com>
2022-11-11 21:14:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd 
 
-st.title(**PCA y Cluster con Kmeans, Indice de Felicidad Banco Mundial**)
+st.title('**PCA y Cluster con Kmeans, Indice de Felicidad Banco Mundial**')
 st.write(''' test
 
 ''')
